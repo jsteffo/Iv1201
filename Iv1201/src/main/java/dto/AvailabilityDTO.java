baa95b2f14a395857c60fc6ea8633ab80b8fa79e
Stefan Josefsson <jsteffo@gmail.com>
2015-02-20 19:11:31 +0100

    Created 2 new DTO. To handle person and availability

---
+++ Iv1201/src/main/java/dto/AvailabilityDTO.java
@@ -0,0 +1,22 @@
+package dto;
+
+import java.time.LocalDate;
+
+public class AvailabilityDTO {
+
+	private LocalDate fromDate;
+	private LocalDate toDate;
+	
+	public LocalDate getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(LocalDate fromDate) {
+		this.fromDate = fromDate;
+	}
+	public LocalDate getToDate() {
+		return toDate;
+	}
+	public void setToDate(LocalDate toDate) {
+		this.toDate = toDate;
+	}
+}
---
+++ Iv1201/src/main/java/dto/PersonDTO.java
@@ -0,0 +1,48 @@
+package dto;
+
+public class PersonDTO {
+
+	private String firstName;
+	private String lastName;
+	private String ssn;
+	private String email;
+	private String password;
+	private String userName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
